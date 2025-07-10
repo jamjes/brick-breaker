a6da13113d84557f2ea4a32ed772f17f1c39051c
Jam <77445181+jamjes@users.noreply.github.com>
2025-07-10 09:12:43 +0100

    Implement paddle mechanics
    Pressing the arrow keys will apply a force to the paddle in that direction.

--- brickBreaker/Assets/_Scripts/Paddle.cs
+++ brickBreaker/Assets/_Scripts/Paddle.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D rigidBody;
+    [SerializeField] private float force = 25f;
+    private int direction;
+
+    private void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (direction == 0)
+        {
+            return;
+        }
+
+        rigidBody.AddForce(new Vector2(direction * force, 0));
     }
 }
